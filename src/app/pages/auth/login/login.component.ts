291fae569c0516481870ff4124d246d019b1f180
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-02-12 14:28:32 -0300

    [FIX]: ajustes na feat de login

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -21,8 +22,14 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
     private router = inject(Router);
+    private fb = inject(FormBuilder);
 
     protected visibility: boolean = false;
+
+    form = this.fb.group({
+        sus: ['', [Validators.required, Validators.pattern('^[0-9]{15}$')]],
+        senha: ['', [Validators.required]],
+    });
 
     redirectToSign() {
         this.router.navigateByUrl('signup');
