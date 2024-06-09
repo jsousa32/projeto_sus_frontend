197839dc8e4ed79a84bafda19972cee2d872d197
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-06-09 17:23:06 -0300

    [FIX]: modificação na estrutura dos inputs da dela de login

--- src/app/pages/external/login/login.component.ts
+++ src/app/pages/external/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
+import { loginForm } from '../../../core/forms/login.forms.model';
 import { ButtonsComponent } from '../../../shared/buttons/buttons.component';
 import { CarouselComponent } from '../../../shared/carousel/carousel.component';
 import { InputTextComponent } from '../../../shared/inputs/input-text/input-text.component';
@@ -12,19 +13,30 @@ import { InputsComponent } from '../../../shared/inputs/inputs.component';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, CarouselComponent, RouterLink, InputTextModule, ReactiveFormsModule, ButtonsComponent, PasswordModule, InputsComponent, InputTextComponent],
+  imports: [
+    CommonModule,
+    CarouselComponent,
+    RouterLink,
+    InputTextModule,
+    ReactiveFormsModule,
+    ButtonsComponent,
+    PasswordModule,
+    InputsComponent,
+    InputTextComponent,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   private fb = inject(FormBuilder);
 
-  protected formsLogin = this.fb.group({
+  protected loginForm = loginForm;
+  protected forms = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required]],
   });
 
   salvar() {
-    console.log(this.formsLogin)
+    console.log(this.forms);
   }
 }
