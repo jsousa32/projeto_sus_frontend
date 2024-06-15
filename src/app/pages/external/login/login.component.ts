10b30e2825ef591c51e1186b97e48033d5eb0b8e
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-06-15 17:19:11 -0300

    [FIX]: correção nas importações

--- src/app/pages/external/login/login.component.ts
+++ src/app/pages/external/login/login.component.ts
@@ -2,12 +2,9 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { InputTextModule } from 'primeng/inputtext';
-import { PasswordModule } from 'primeng/password';
 import { loginForm } from '../../../core/forms/login.forms.model';
 import { ButtonsComponent } from '../../../shared/buttons/buttons.component';
 import { CarouselComponent } from '../../../shared/carousel/carousel.component';
-import { InputTextComponent } from '../../../shared/inputs/input-text/input-text.component';
 import { InputsComponent } from '../../../shared/inputs/inputs.component';
 
 @Component({
@@ -17,12 +14,9 @@ import { InputsComponent } from '../../../shared/inputs/inputs.component';
     CommonModule,
     CarouselComponent,
     RouterLink,
-    InputTextModule,
     ReactiveFormsModule,
     ButtonsComponent,
-    PasswordModule,
     InputsComponent,
-    InputTextComponent,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
