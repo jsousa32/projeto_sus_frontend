d75f288d9788c114e74498285a4e064b750027be
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-06-25 12:14:10 -0300

    [FIX]: correção gap layout

--- src/app/pages/internal/layout/layout.component.ts
+++ src/app/pages/internal/layout/layout.component.ts
@@ -9,10 +9,10 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   standalone: true,
   imports: [CommonModule, NavbarComponent, SidebarComponent, RouterOutlet],
   template: `
-    <div class="flex flex-col h-screen bg-primary/30">
+    <div class="flex flex-col h-screen overflow-x-auto bg-primary/30">
       <app-navbar (toggled)="toggledSidebar = $event" />
 
-      <div class="flex h-full p-[1rem] gap-[1rem]">
+      <div class="flex h-full p-[1rem]" [ngClass]="{ 'gap-[1rem]': toggledSidebar }">
         <app-sidebar [toggledSidebar]="toggledSidebar" />
 
         <div class="w-full">
