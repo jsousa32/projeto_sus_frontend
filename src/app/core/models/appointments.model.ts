54f9ccdbe431871246e00e6093647d905561f498
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-12 15:45:48 -0300

    [ADD]: adicionado novos models as consultas

--- src/app/core/models/appointments.model.ts
+++ src/app/core/models/appointments.model.ts
@@ -1,9 +1,14 @@
 import { Doctor } from './doctors.model.dto';
-import { Pacient } from './pacient.model.dto';
 
-export interface Appointments {
+export interface Appointment {
+  id?: string;
   date: Date;
   hour: string;
   doctor: Doctor;
-  pacient: Pacient;
 }
+
+export interface AppointmentPage extends Appointment {}
+
+export interface AppointmentCreate extends Omit<Appointment, 'id' | 'doctor'> {}
+
+export interface AppointmentEditableFields extends Omit<Appointment, 'id' | 'doctor'> {}
