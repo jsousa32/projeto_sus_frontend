ca7ff820fa3f5b2933a96e0461d9ca77f65fe038
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-12 14:35:49 -0300

    [ADD]: adição de novos models ao pacient

--- src/app/core/models/pacient.model.dto.ts
+++ src/app/core/models/pacient.model.dto.ts
@@ -1,6 +1,16 @@
 export interface Pacient {
-  name: string;
-  susNumber: string;
+  id?: string;
+  firstName: string;
+  lastName: string;
   email: string;
+  password: string;
   telephone: string;
+  document: string;
+  susNumber: string;
 }
+
+export interface PacientPage extends Omit<Pacient, 'password'> {}
+
+export interface PacientResume extends Omit<Pacient, 'password'> {}
+
+export interface PacientEditableFields extends Omit<Pacient, 'id' | 'password' | 'document' | 'susNumber'> {}
