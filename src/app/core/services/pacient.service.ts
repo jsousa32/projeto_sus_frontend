cdb5a5ed2b94f20951aba5a4465447c170d3c24e
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-12 14:36:00 -0300

    [FEAT]: criação do service de paciente

---
+++ src/app/core/services/pacient.service.ts
@@ -0,0 +1,81 @@
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { catchError, take } from 'rxjs';
+import { Pacient, PacientEditableFields, PacientPage, PacientResume } from '../models/pacient.model.dto';
+import { Page } from '../utils/page.utils';
+import { CatchErrorHandler } from './catch-error.handler';
+import { Routes } from './routes-back';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PacientService {
+  private httpClient = inject(HttpClient);
+
+  save(pacient: Pacient) {
+    return this.httpClient.post(Routes.RoutesPacients.SAVE, pacient).pipe(
+      take(1),
+      catchError((err: HttpErrorResponse) => CatchErrorHandler.err(err))
+    );
+  }
+
+  allPacients(params: HttpParams) {
+    return this.httpClient
+      .get<Page<PacientPage>>(Routes.RoutesPacients.ALL_PACIENTS, {
+        params: params,
+      })
+      .pipe(take(1));
+  }
+
+  allPacientsUnpaged(params: HttpParams) {
+    return this.httpClient
+      .get<Page<PacientPage>>(Routes.RoutesPacients.ALL_PACIENTS_UNPAGED, {
+        params: params,
+      })
+      .pipe(take(1));
+  }
+
+  pacient(id: string) {
+    return this.httpClient
+      .get<PacientResume>(Routes.RoutesPacients.PACIENT, {
+        params: new HttpParams().append('id', id),
+      })
+      .pipe(
+        take(1),
+        catchError((err: HttpErrorResponse) => CatchErrorHandler.err(err))
+      );
+  }
+
+  update(id: string, pacient: PacientEditableFields) {
+    return this.httpClient
+      .patch(Routes.RoutesPacients.UPDATE_PACIENT, pacient, {
+        params: new HttpParams().append('id', id),
+      })
+      .pipe(
+        take(1),
+        catchError((err: HttpErrorResponse) => CatchErrorHandler.err(err))
+      );
+  }
+
+  active(id: string) {
+    return this.httpClient
+      .patch(Routes.RoutesPacients.ACTIVE_PACIENT, null, {
+        params: new HttpParams().append('id', id),
+      })
+      .pipe(
+        take(1),
+        catchError((err: HttpErrorResponse) => CatchErrorHandler.err(err))
+      );
+  }
+
+  disable(id: string) {
+    return this.httpClient
+      .delete(Routes.RoutesPacients.DESACTIVE_PACIENT, {
+        params: new HttpParams().append('id', id),
+      })
+      .pipe(
+        take(1),
+        catchError((err: HttpErrorResponse) => CatchErrorHandler.err(err))
+      );
+  }
+}
