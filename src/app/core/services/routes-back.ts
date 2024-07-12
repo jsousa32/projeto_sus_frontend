49c86fd6bbc790c7b6c7215fe1d9c86108d164d3
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-12 14:21:44 -0300

    [FIX]: alterado a visibilidade dos atributos

--- src/app/core/services/routes-back.ts
+++ src/app/core/services/routes-back.ts
@@ -6,40 +6,40 @@ export class Routes {
 
   static RoutesAutentications = class {
     private static BASE = Routes.url + '/auth';
-    private static LOGIN = this.BASE;
-    private static EMAIL_CONFIRMATION = this.BASE + '/email-confirmation';
-    private static FORGOT = this.BASE + '/forgot';
-    private static RESET = this.BASE + '/reset';
+    public static LOGIN = this.BASE;
+    public static EMAIL_CONFIRMATION = this.BASE + '/email-confirmation';
+    public static FORGOT = this.BASE + '/forgot';
+    public static RESET = this.BASE + '/reset';
   }
 
   static RoutesPacients = class {
     private static BASE = Routes.url + '/pacients';
-    private static SAVE = this.BASE;
-    private static ALL_PACIENTS = this.BASE + '/all';
-    private static ALL_PACIENTS_UNPAGED = this.BASE + '/unpaged';
-    private static PACIENT = this.BASE;
-    private static UPDATE_PACIENT = this.BASE;
-    private static ACTIVE_PACIENT = this.BASE + '/active';
-    private static DESACTIVE_PACIENT = this.BASE;
+    public static SAVE = this.BASE;
+    public static ALL_PACIENTS = this.BASE + '/all';
+    public static ALL_PACIENTS_UNPAGED = this.BASE + '/unpaged';
+    public static PACIENT = this.BASE;
+    public static UPDATE_PACIENT = this.BASE;
+    public static ACTIVE_PACIENT = this.BASE + '/active';
+    public static DESACTIVE_PACIENT = this.BASE;
   }
 
   static RoutesDoctors = class {
     private static BASE = Routes.url + '/doctors';
-    private static SAVE = this.BASE;
-    private static ALL_DOCTORS = this.BASE + '/all';
-    private static ALL_DOCTORS_UNPAGED = this.BASE + '/unpaged';
-    private static DOCTOR = this.BASE;
-    private static UPDATE_DOCTOR = this.BASE;
-    private static ACTIVE_DOCTOR = this.BASE + '/active';
-    private static DESACTIVE_DOCTOR = this.BASE;
+    public static SAVE = this.BASE;
+    public static ALL_DOCTORS = this.BASE + '/all';
+    public static ALL_DOCTORS_UNPAGED = this.BASE + '/unpaged';
+    public static DOCTOR = this.BASE;
+    public static UPDATE_DOCTOR = this.BASE;
+    public static ACTIVE_DOCTOR = this.BASE + '/active';
+    public static DESACTIVE_DOCTOR = this.BASE;
   }
 
   static RoutesAdmins = class {
     private static BASE = Routes.url + '/admins';
-    private static SAVE = this.BASE;
-    private static ALL_ADMINS = this.BASE + '/all';
-    private static ADMIN = this.BASE;
-    private static UPDATE_ADMIN = this.BASE;
-    private static DELETE_ADMIN = this.BASE;
+    public static SAVE = this.BASE;
+    public static ALL_ADMINS = this.BASE + '/all';
+    public static ADMIN = this.BASE;
+    public static UPDATE_ADMIN = this.BASE;
+    public static DELETE_ADMIN = this.BASE;
   }
 }
