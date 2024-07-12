fd083650a0924f8c8a9c0c5c7a11831f51201e26
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-12 17:58:41 -0300

    [FEAT]: implementação da funcionalidade de login

--- src/app/pages/external/login/login.component.ts
+++ src/app/pages/external/login/login.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { tap } from 'rxjs';
 import { loginForm } from '../../../core/forms/login.forms.model';
+import { AuthService } from '../../../core/services/auth.service';
 import { ButtonsComponent } from '../../../shared/buttons/buttons.component';
 import { CarouselComponent } from '../../../shared/carousel/carousel.component';
 import { InputsComponent } from '../../../shared/inputs/inputs.component';
@@ -10,19 +12,14 @@ import { InputsComponent } from '../../../shared/inputs/inputs.component';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    CommonModule,
-    CarouselComponent,
-    RouterLink,
-    ReactiveFormsModule,
-    ButtonsComponent,
-    InputsComponent,
-  ],
+  imports: [CommonModule, CarouselComponent, RouterLink, ReactiveFormsModule, ButtonsComponent, InputsComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
   protected loginForm = loginForm;
   protected forms = this.fb.group({
@@ -30,7 +27,10 @@ export class LoginComponent {
     password: ['', [Validators.required]],
   });
 
-  salvar() {
-    console.log(this.forms);
+  login() {
+    this.authService
+      .login(this.forms.value.email!, this.forms.value.password!)
+      .pipe(tap((_) => this.router.navigate(['/dashboard'])))
+      .subscribe();
   }
 }
