11bbc2f4edcb950e571001547b6c19ef94a1d465
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-12 16:51:07 -0300

    [FEAT]: implementação do serviço de autenticação

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,79 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { catchError, take, tap } from 'rxjs';
+import { AuthResponse } from '../models/auth-response.model.dto';
+import { CryptoUtils } from '../utils/crypto.utils';
+import { StorageUtils } from '../utils/storage.utils';
+import { CatchErrorHandler } from './catch-error.handler';
+import { Routes } from './routes-back';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private httpClient = inject(HttpClient);
+
+  login(email: string, password: string) {
+    return this.httpClient
+      .post<AuthResponse>(Routes.RoutesAuthentications.LOGIN, null, {
+        headers: this.buildHeaderBasicAuth(email, password),
+      })
+      .pipe(
+        take(1),
+        tap((res) => StorageUtils.add('userSession', res)),
+        catchError((err: HttpErrorResponse) => CatchErrorHandler.err(err))
+      );
+  }
+
+  emailConfirmation(codeEmailConfirmation: string) {
+    return this.httpClient
+      .post(Routes.RoutesAuthentications.EMAIL_CONFIRMATION, null, {
+        params: new HttpParams().append('codeEmailConfirmation', codeEmailConfirmation),
+      })
+      .pipe(
+        take(1),
+        tap((_) => {
+          const userSession = StorageUtils.find('userSession') as AuthResponse;
+
+          userSession.emailConfirmed = true;
+
+          StorageUtils.add('userSession', userSession);
+        }),
+        catchError((err: HttpErrorResponse) => CatchErrorHandler.err(err))
+      );
+  }
+
+  forgot(email: string) {
+    return this.httpClient
+      .post(Routes.RoutesAuthentications.FORGOT, null, {
+        params: new HttpParams().append('email', email),
+      })
+      .pipe(
+        take(1),
+        catchError((err: HttpErrorResponse) => CatchErrorHandler.err(err))
+      );
+  }
+
+  reset(forgotId: string, userId: string, password: string) {
+    return this.httpClient
+      .post(Routes.RoutesAuthentications.RESET, null, {
+        params: new HttpParams()
+          .append('forgotId', forgotId)
+          .append('userId', userId)
+          .append('password', CryptoUtils.encrypt(password)),
+      })
+      .pipe(
+        take(1),
+        catchError((err: HttpErrorResponse) => CatchErrorHandler.err(err))
+      );
+  }
+
+  private buildHeaderBasicAuth(email: string, senha: string) {
+    let headers = new HttpHeaders();
+
+    headers = headers.append('Content-Type', 'application/json');
+    headers = headers.append('Authorization', 'Basic ' + window.btoa(email + ':' + senha));
+
+    return headers;
+  }
+}
