4ecf4b4880ee8211ea9a8e459a94a8f12402c6b2
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-13 20:37:31 -0300

    [FEAT]: implementação do resetar senha

--- src/app/pages/external/reset/reset.component.ts
+++ src/app/pages/external/reset/reset.component.ts
@@ -1,8 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { filter, finalize, map, tap } from 'rxjs';
 import { resetForm } from '../../../core/forms/reset.forms.model';
+import { AuthService } from '../../../core/services/auth.service';
+import { CryptoUtils } from '../../../core/utils/crypto.utils';
+import { SwalertUtils } from '../../../core/utils/swalert.utils';
+import { PasswordValidator } from '../../../core/validators/password.validator';
 import { ButtonsComponent } from '../../../shared/buttons/buttons.component';
 import { CarouselComponent } from '../../../shared/carousel/carousel.component';
 import { InputsComponent } from '../../../shared/inputs/inputs.component';
@@ -16,12 +21,49 @@ import { InputsComponent } from '../../../shared/inputs/inputs.component';
 })
 export default class ResetComponent {
   private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private activatedRouter = inject(ActivatedRoute);
+  private authService = inject(AuthService);
+  private userId = '';
 
+  protected loading = false;
   protected resetForm = resetForm;
-  protected forms = this.fb.group({
-    password: ['', [Validators.required]],
-    confirmPassword: ['', [Validators.required]],
-  });
+  protected forms = this.fb.group(
+    {
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]],
+    },
+    { validators: [PasswordValidator] }
+  );
 
-  reset() {}
+  protected params$ = this.activatedRouter.paramMap.pipe(
+    map((params) => ({
+      userId: params.get('userId'),
+      expiresAt: params.get('expiresAt'),
+    })),
+    filter(({ userId, expiresAt }) => !!userId && !!expiresAt),
+    tap(({ userId, expiresAt }) => {
+      this.userId = atob(userId!);
+      this.validationUrl(atob(expiresAt!));
+    })
+  );
+
+  reset() {
+    this.authService
+      .reset(this.userId, this.forms.value.password!)
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe(() => {
+        SwalertUtils.swalertSuccessWithoutOptions('Parabéns', 'Senha redefinida com sucesso').then((confirm) => {
+          if (confirm) this.router.navigate(['login']);
+        });
+      });
+  }
+
+  validationUrl(expiresAt: string) {
+    if (new Date(CryptoUtils.decrypt(expiresAt)) < new Date()) {
+      SwalertUtils.swalertError('Link Expirado', 'O link solicitado já está expirado.').then((confirm) => {
+        if (confirm) this.router.navigate(['login']);
+      });
+    }
+  }
 }
