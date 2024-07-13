82dea3721eadcdc26b15a7e293702f84a8a066dc
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-13 20:36:37 -0300

    [FIX]: correção no método de criptografia

--- src/app/core/utils/crypto.utils.ts
+++ src/app/core/utils/crypto.utils.ts
@@ -3,11 +3,17 @@ import { environment } from '../../../environments/environment';
 
 export class CryptoUtils {
   static encrypt(data: string) {
-    return CryptoJS.AES.encrypt(data, environment.secretKey).toString();
+    return CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(environment.secretKey), {
+      mode: CryptoJS.mode.ECB,
+      padding: CryptoJS.pad.Pkcs7,
+    }).toString();
   }
 
   static decrypt(data: string) {
-    const bytes = CryptoJS.AES.decrypt(data, environment.secretKey);
+    const bytes = CryptoJS.AES.decrypt(data, CryptoJS.enc.Utf8.parse(environment.secretKey), {
+      mode: CryptoJS.mode.ECB,
+      padding: CryptoJS.pad.Pkcs7,
+    });
 
     return bytes.toString(CryptoJS.enc.Utf8);
   }
