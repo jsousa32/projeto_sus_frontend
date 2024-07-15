93d051af0839ea95564f50e804f838d89a8e72d2
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-15 20:53:55 -0300

    [REFACTOR]: refatoração do componente input de texto

---
+++ src/app/shared/inputs/input-text/input-text.component.ts
@@ -0,0 +1,72 @@
+import { CommonModule } from '@angular/common';
+import { Component, forwardRef, inject, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, FormControl, FormGroupDirective, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+
+@Component({
+  selector: 'app-input-text',
+  standalone: true,
+  imports: [CommonModule, FormsModule, InputTextModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputTextComponent),
+      multi: true,
+    },
+  ],
+  templateUrl: './input-text.component.html',
+  styleUrl: './input-text.component.scss',
+})
+export class InputTextComponent implements OnInit, ControlValueAccessor {
+  private formGroupDirective = inject(FormGroupDirective);
+  private innerValue: any;
+
+  protected controls!: FormControl;
+
+  @Input({ required: true })
+  id = '';
+
+  @Input({ required: true })
+  label = '';
+
+  @Input()
+  required = false;
+
+  @Input()
+  readonly = false;
+
+
+  get value() {
+    return this.innerValue;
+  }
+
+  set value(v: any) {
+    if (this.innerValue !== v) {
+      this.innerValue = v;
+      this.onChaged(v);
+    }
+  }
+
+  onChaged: (_: any) => void = () => {};
+  onTouched: (_: any) => void = () => {};
+
+  ngOnInit(): void {
+    this.controls = this.formGroupDirective.control.get(this.id) as FormControl;
+  }
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChaged = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.readonly = isDisabled;
+  }
+}
