d02576e9b27ff6028e77114f2b0eb89cbaf4062d
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-15 21:13:58 -0300

    [REFACTOR]: refatoração do input de mascaras

---
+++ src/app/shared/inputs/input-mask/input-mask.component.ts
@@ -0,0 +1,67 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, FormControl, FormGroupDirective, FormsModule } from '@angular/forms';
+import { InputMaskModule } from 'primeng/inputmask';
+
+@Component({
+  selector: 'app-input-mask',
+  standalone: true,
+  imports: [CommonModule, FormsModule, InputMaskModule],
+  templateUrl: './input-mask.component.html',
+  styleUrl: './input-mask.component.scss',
+})
+export class InputMaskComponent implements OnInit, ControlValueAccessor {
+  private formGroupDirective = inject(FormGroupDirective);
+  private innerValue: any;
+
+  protected controls!: FormControl;
+
+  @Input({ required: true })
+  id = '';
+
+  @Input({ required: true })
+  label = '';
+
+  @Input({ required: true })
+  mask = '';
+
+  @Input()
+  required = false;
+
+  @Input()
+  readonly = false;
+
+  get value() {
+    return this.innerValue;
+  }
+
+  set value(v: any) {
+    if (this.innerValue !== v) {
+      this.innerValue = v;
+      this.onChaged(v);
+    }
+  }
+
+  onChaged: (_: any) => void = () => {};
+  onTouched: (_: any) => void = () => {};
+
+  ngOnInit(): void {
+    this.controls = this.formGroupDirective.control.get(this.id) as FormControl;
+  }
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChaged = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.readonly = isDisabled;
+  }
+}
