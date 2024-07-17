11eefc48ea30d9cb9f2cb1f2dd2cebf2ec8dddc5
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-17 01:57:47 -0300

    [ADD]: rota para criação de uma consulta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -94,9 +94,15 @@ export const routes: Routes = [
       },
       {
         path: 'appointments',
-        data: { permissions: [Permissions.ADMIN] },
+        data: { permissions: [Permissions.ADMIN, Permissions.PACIENT] },
         loadComponent: () =>
           import('../app/pages/internal/appointments/listing-appointments/listing-appointments.component'),
+      },
+      {
+        path: 'appointments/new',
+        data: { permissions: [Permissions.ADMIN, Permissions.PACIENT] },
+        loadComponent: () =>
+          import('../app/pages/internal/appointments/create-and-edit-appointment/create-and-edit-appointment.component'),
       },
     ],
   },
