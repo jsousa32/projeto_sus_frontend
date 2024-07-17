d15a5c5ee4fc1dd8002da41df0e3124a9bc4f5d0
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-17 01:57:28 -0300

    [ADD]: adicionado id a sessão do usuário

--- src/app/core/models/user-session.model.dto.ts
+++ src/app/core/models/user-session.model.dto.ts
@@ -2,6 +2,7 @@ import { Permissions } from '../enums/permissions.enum';
 
 export interface UserSession {
   accessToken: string;
+  id: string;
   emailConfirmed: boolean;
   permissions: Permissions[];
   name: string;
