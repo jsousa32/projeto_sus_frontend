6c3b8c4666e4a4d72be4c3171e0697f7d5bcf0d8
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-17 01:55:14 -0300

    [REFACTOR]: refatoração na classe de permissão

--- src/app/core/utils/permission.utils.ts
+++ src/app/core/utils/permission.utils.ts
@@ -1,15 +1,19 @@
 import { Permissions } from '../enums/permissions.enum';
+import { UserSession } from '../models/user-session.model.dto';
+import { StorageUtils } from './storage.utils';
 
 export class PermissionsUtils {
-  static isAdmin(permissions: Permissions[]) {
-    return permissions.some((perm) => perm == Permissions.ADMIN);
+  static permissions = (StorageUtils.find('userSession') as UserSession).permissions;
+
+  static isAdmin() {
+    return PermissionsUtils.permissions.some((perm) => perm == Permissions.ADMIN);
   }
 
-  static isDoctor(permissions: Permissions[]) {
-    return permissions.some((perm) => perm == Permissions.DOCTOR);
+  static isDoctor() {
+    return PermissionsUtils.permissions.some((perm) => perm == Permissions.DOCTOR);
   }
 
-  static isPacient(permissions: Permissions[]) {
-    return permissions.some((perm) => perm == Permissions.PACIENT);
+  static isPacient() {
+    return PermissionsUtils.permissions.some((perm) => perm == Permissions.PACIENT);
   }
 }
