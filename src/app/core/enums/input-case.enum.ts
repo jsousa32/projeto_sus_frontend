d9c50b3a472c0b668e3a0dca53a2fdef68117656
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-08-11 15:02:17 -0300

    [REMOVE]: remoção do enum  e model de inputs

--- src/app/core/enums/input-case.enum.ts
+++
@@ -1,9 +0,0 @@
-export enum InputCase {
-  TEXT = 'text',
-  PASSWORD = 'password',
-  SUS = 'susNumber',
-  TELEPHONE = 'telephone',
-  DOCUMENT = 'document',
-  TOKEN = 'token',
-  CRM = 'crm'
-}
--- src/app/core/models/input.model.dto.ts
+++
@@ -1,9 +0,0 @@
-import { InputCase } from '../enums/input-case.enum';
-
-export interface Input {
-  case: InputCase;
-  formControlName: string;
-  placeholder: string;
-  required?: boolean;
-  readonly?: boolean;
-}
